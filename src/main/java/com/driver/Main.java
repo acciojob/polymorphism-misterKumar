732b94ba955eb13b18257764b9878e462b9a029d
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-01-13 12:06:12 +0530

    firstcommit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public void main(String[]args) {
+        Product p = new Product();
+        p.product(4, 5);
+        p.product(4,5,6);
+        p.product(4.5,6.5);
+    }
+class Product{
+    public int product (int x,int y){
+        return x*y;
+    }
+    public int product(int x, int y, int z) {
+        return x*y*z;
+    }
+    public double product(double x, double y) {
+        return x*y;
+    }
+}
 }
